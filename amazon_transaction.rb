ed9d09ec919dd4ddb8f44782670dc5f3c3658213
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-30 17:42:44 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,22 @@
 # Code your cash register here!
+class AmazonTransaction
+  
+  attr_accessor :total, :items, :discount, :title, :price
+  
+  def initialize(title, price, discount)
+    @total = 0
+    @items = []
+    @title = title
+    @price = price
+    @discount = discount
+  end
+  def purchase 
+    @total += @price
+   end
+end
+
+shoes = AmazonTransaction.new("Shoes", "5.99", ".02")
+puts shoes.price
+puts shoes.total
+purchase
+puts shoes.total
